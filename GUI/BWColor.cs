918f0f2680eb6fd4ac1e7872f448ec0d3d54133f
Evgeny <62696210+MiniGerkules@users.noreply.github.com>
2022-05-12 22:32:24 +0300

    Add classes that describe colors

---
+++ GUI/BWColor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    /// <summary>
+    /// The class describes the black and white color
+    /// </summary>
+    class BWColor
+    {
+    }
+}
---
+++ GUI/Color.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace GUI
+{
+    /// <summary>
+    /// The class describes a color based on cyan, magenta, yellow, blue and white.
+    /// </summary>
+    class Color
+    {
+        private uint cyan, magenta, yellow, blue, white;
+
+        public Color() : this((0, 0, 0, 0, 0))
+        {
+        }
+
+        public Color((uint cyan, uint magenta, uint yellow, uint blue, uint white) colors)
+        {
+            (cyan, magenta, yellow, blue, white) = colors;
+        }
+
+        public Color(string[] colors)
+        {
+            (cyan, magenta, yellow, blue, white) = colors.Select(elem => uint.Parse(elem)).ToArray() switch { 
+                var arr when arr.Length == 5 => (arr[0], arr[1], arr[2], arr[3], arr[4]),
+                _ => throw new ArgumentException("The number of colors is not equal to 5!")
+            };
+        }
+
+        //public static explicit operator BWColor(Color color)
+        //{
+
+        //}
+    }
+}
