d4517d07de965f8db59b20d22e0747c929cd0dd1
Evgeny <62696210+MiniGerkules@users.noreply.github.com>
2022-05-12 22:33:00 +0300

    Add class that describes the stroke

---
+++ GUI/Stroke.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace GUI
+{
+    class Stroke
+    {
+        private readonly Vector<uint> start;
+        private readonly Vector<uint> end;
+        private readonly Color color;
+
+        public Stroke(Vector<uint> start, Vector<uint> end, Color color)
+        {
+            this.start = start;
+            this.end = end;
+            this.color = color;
+        }
+    }
+}
