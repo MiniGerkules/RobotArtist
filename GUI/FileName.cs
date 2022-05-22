31b3d20b3dcb60112b8ae08ee341a58bcff67ea9
Evgeny <62696210+MiniGerkules@users.noreply.github.com>
2022-05-22 21:44:48 +0300

    Add FileName class

---
+++ GUI/FileName.cs
@@ -0,0 +1,18 @@
+﻿namespace GUI
+{
+    internal class FileName
+    {
+        public string ShortName { get; private set; }
+        public string FullName { get; private set; }
+
+        public FileName(string fullName) : this(fullName, Helpers.GetFileName(fullName))
+        {
+        }
+
+        public FileName(string fullName, string shortName)
+        {
+            FullName = fullName;
+            ShortName = shortName;
+        }
+    }
+}
