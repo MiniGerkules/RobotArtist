16ca5cf4fa4d36ea3e9d3721cc84155fd023cb5c
Evgeny <62696210+MiniGerkules@users.noreply.github.com>
2022-05-22 21:40:59 +0300

    Add a wrapper over RenderTargetBitmap

---
+++ GUI/RotatedBitmap.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace GUI
+{
+    internal class RotatedBitmap
+    {
+        public RenderTargetBitmap Bitmap { get; private set; }
+        public ushort Angle { get; private set; }
+
+        public RotatedBitmap(RenderTargetBitmap bitmap)
+        {
+            Bitmap = bitmap;
+            Angle = 0;
+        }
+
+        public void Rotate()
+        {
+            Angle = (ushort)((Angle + 90) % 360);
+        }
+    }
+}
