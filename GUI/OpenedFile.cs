78779f1a41347326969be19d79cf0dfc1cd66cbd
Evgeny <62696210+MiniGerkules@users.noreply.github.com>
2022-05-22 21:45:15 +0300

    Add a wrapper over Button

---
+++ GUI/OpenedFile.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GUI
+{
+    internal class OpenedFile : Button
+    {
+        private string fullName;
+        public string FullName
+        {
+            get => fullName;
+            set
+            {
+                fullName = value;
+                ShortName = Helpers.GetFileName(value);
+                Content = ShortName;
+            }
+        }
+        public string ShortName { get; private set; }
+
+        public OpenedFile(string fullName) : this(fullName, Helpers.GetFileName(fullName))
+        {
+        }
+
+        public OpenedFile(string fullName, string shortName)
+        {
+            FullName = fullName;
+            Content = shortName;
+
+            Margin = new Thickness(5, 5, 5, 5);
+            FontSize = 16;
+        }
+    }
+}
