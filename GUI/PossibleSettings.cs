7483bc6b12b9b97f113fd6478f2c6595a105ed37
Evgeny <62696210+MiniGerkules@users.noreply.github.com>
2022-05-23 15:38:15 +0300

    Add PossibleSettings enum

---
+++ GUI/PossibleSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    internal enum PossibleSettings
+    {
+        itersMinOverlap,
+        minOverlap,
+        maxOverlap,
+        pixTol,
+        pixTol2,
+        pixTolBest,
+        maxLen,
+        brushWidth
+    }
+}
