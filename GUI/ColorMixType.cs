e9e676ecd345845dd3352d1f3190d29519a827a3
Evgeny <62696210+MiniGerkules@users.noreply.github.com>
2022-08-07 14:21:00 +0300

    Add all possible mixing types

---
+++ GUI/ColorMixType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    internal enum ColorMixType
+    {
+        MagentaYellow = 0, YellowCyan, CyanMagenta
+    }
+}
