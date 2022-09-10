854d5cb23ad1c73f1fff94602e099f6396da3356
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-09-10 23:25:45 +0300

    Add 3d matrix

---
+++ GUI/Matrix3D.cs
@@ -0,0 +1,17 @@
+﻿namespace GUI {
+    internal class Matrix3D {
+        private Matrix[] matrix;
+
+        public Matrix this[int layer] {
+            get => matrix[layer];
+            set => matrix[layer] = value;
+        }
+
+        public Matrix3D(int rows, int columns, int layers) {
+            matrix = new Matrix[layers];
+
+            for (int i = 0; i < layers; ++i)
+                matrix[i] = new(rows, columns);
+        }
+    }
+}
