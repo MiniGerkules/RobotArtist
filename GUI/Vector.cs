81dec9977f52856061be69e21a0ba4cdfc2f60be
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-09-10 23:29:45 +0300

    Add a vector

---
+++ GUI/Vector.cs
@@ -0,0 +1,20 @@
+﻿namespace GUI {
+    internal class Vector {
+        private double[] vector;
+        public bool IsRow { get; private set; }
+
+        public double this[int index] {
+            get => vector[index];
+            set => vector[index] = value;
+        }
+
+        public Vector(int size, bool isRow = true) {
+            vector = new double[size];
+            IsRow = isRow;
+        }
+
+        public void Transpose() {
+            IsRow = !IsRow;
+        }
+    }
+}
