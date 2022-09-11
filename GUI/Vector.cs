a14df39958bd202a0d08e624b6ac79199d6b7e76
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-09-11 21:48:20 +0300

    Add vector

--- GUI/Vector.cs
+++ GUI/Vector.cs
@@ -1,6 +1,10 @@
-﻿namespace GUI {
+﻿using System;
+using System.Collections.Generic;
+
+namespace GUI {
     internal class Vector {
-        private double[] vector;
+        private readonly double[] vector;
+        public int Size { get => vector.Length; }
         public bool IsRow { get; private set; }
 
         public double this[int index] {
@@ -9,8 +13,32 @@
         }
 
         public Vector(int size, bool isRow = true) {
+            if (size == 0)
+                throw new ArgumentException("Can't create an empty vector!");
+
             vector = new double[size];
             IsRow = isRow;
+        }
+
+        public Vector(double[] vector, bool isRow = true) {
+            if (vector.Length == 0)
+                throw new ArgumentException("Can't create an empty vector!");
+
+            this.vector = new double[vector.Length];
+            IsRow = isRow;
+            Array.Copy(vector, this.vector, vector.Length);
+        }
+
+        public Vector(List<double> vector, bool isRow = true) {
+            if (vector.Count == 0)
+                throw new ArgumentException("Can't create an empty vector!");
+
+            this.vector = vector.ToArray();
+            IsRow = isRow;
+        }
+
+        public Vector(Vector other) : this(other.vector) {
+            IsRow = other.IsRow;
         }
 
         public void Transpose() {
