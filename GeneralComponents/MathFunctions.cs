52d36376d6e6b4fdaea60051e64a1212d747a070
SinBar <varka.sinitsckaya@yandex.ru>
2022-11-07 13:59:55 +0300

    Add missing files

---
+++ GeneralComponents/MathFunctions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeneralComponents {
+    public static class MathFunctions {
+        public static double Plus(double a, double b) => a + b;
+        public static double Minus(double a, double b) => a - b;
+        public static double Multiply(double a, double b) => a * b;
+        public static double Divide(double a, double b) => a / b;
+        public static double Square(double a) => a * a;
+        public static double Sqrt(double a) => Math.Sqrt(a);
+    }
+}
