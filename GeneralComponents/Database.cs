0241866e9c436e537746959a1516f222d4229fff
SinBar <varka.sinitsckaya@yandex.ru>
2022-11-07 13:48:00 +0300

    git update database file

--- GeneralComponents/Database.cs
+++ GeneralComponents/Database.cs
@@ -1,59 +1,62 @@
-﻿using System;
-using System.IO;
+﻿using System.Linq;
 using System.Collections.Generic;
-using NPOI.SS.UserModel;
-using NPOI.XSSF.UserModel;
-using NPOI.HSSF.UserModel;
+using System.Collections.Immutable;
 
-namespace GeneralComponents
-{
-    public static class Database
-    {
-        private static List<List<List<double>>> data = null;
-        public static List<List<List<double>>> Data => data;
+namespace GeneralComponents {
+    public class Database {
+        private readonly List<List<List<double>>> data;
 
-        public static bool IsLoad()
-        {
-            return data != null;
+        public Database(List<List<List<double>>> data) {
+            this.data = data;
         }
 
-        public static void LoadDatabase(String pathToColorDatabase)
-        {
-            if (!File.Exists(pathToColorDatabase))
-            {
-                data = null;
-                return;
-            }
+        public ImmutableList<ImmutableList<ImmutableList<double>>> GetHSV() {
+            List<List<List<double>>> hsv = Enumerable.Repeat(new List<List<double>>(), data.Count).ToList();
 
-            IWorkbook workbook;
-            using (FileStream stream = File.OpenRead(pathToColorDatabase))
-            {
-                if (pathToColorDatabase.EndsWith(".xls"))
-                    workbook = new HSSFWorkbook(stream);
-                else
-                    workbook = new XSSFWorkbook(stream);
-            }
-            
-            data = new(workbook.NumberOfSheets);
-            for (int i = 0; i < workbook.NumberOfSheets; ++i)
-                GetSheetIntoDataBase(workbook.GetSheetAt(i));
+            for (int i = 0; i < data.Count; ++i)
+                hsv[i].AddRange(data[i].Select(elem => elem.GetRange(0, 3)));
 
-            workbook.Close();
+            return hsv.ToImmutable();
         }
 
-        private static void GetSheetIntoDataBase(ISheet sheet)
-        {
-            List<List<double>> sheetInList = new(sheet.LastRowNum);
-            foreach (IRow row in sheet)
-            {
-                List<double> rowInList = new(row.LastCellNum);
-                foreach (ICell cell in row)
-                    rowInList.Add(cell.NumericCellValue);
+        public ImmutableList<ImmutableList<ImmutableList<double>>> GetProportions() {
+            List<List<List<double>>> props = Enumerable.Repeat(new List<List<double>>(), data.Count).ToList();
 
-                sheetInList.Add(rowInList);
+            for (int i = 0; i < data.Count; ++i)
+                props[i].AddRange(data[i].Select(elem => elem.GetRange(3, 3)));
+
+            return props.ToImmutable();
+        }
+
+        public ImmutableList<ImmutableList<double>> GetHSV(ColorMixType mixType) {
+            List<List<double>> hsv = new();
+
+            if (mixType == ColorMixType.MagentaYellow1 || mixType == ColorMixType.MagentaYellow2) {
+                hsv = new(data[0].Count + data[1].Count);
+                hsv.AddRange(data[0].Select(elem => elem.GetRange(0, 3)));
+                hsv.ForEach(elem => elem[0] -= 1);
+                hsv.AddRange(data[1].Select(elem => elem.GetRange(0, 3)));
+            } else {
+                hsv = new(data[(int)mixType].Count);
+                hsv.AddRange(data[(int)mixType].Select(elem => elem.GetRange(0, 3)));
             }
 
-            data.Add(sheetInList);
+            return hsv.ToImmutable();
+        }
+
+        public ImmutableList<ImmutableList<double>> GetProportions(ColorMixType mixType) {
+            List<List<double>> proportions = new();
+
+            if (mixType == ColorMixType.MagentaYellow1 || mixType == ColorMixType.MagentaYellow2) {
+                proportions = new(data[0].Count + data[1].Count);
+                proportions.AddRange(data[0].Select(elem => elem.GetRange(3, 3)));
+                proportions.AddRange(data[1].Select(elem => elem.GetRange(3, 3)));
+            } else {
+                proportions = new(data[(int)mixType].Count);
+                proportions.AddRange(data[(int)mixType].Select(elem => elem.GetRange(3, 3)));
+            }
+
+            return proportions.ToImmutable();
         }
     }
 }
