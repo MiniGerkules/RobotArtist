a30a5fb3e36fe786950c5b2f1123c1b5371ddba2
SinBar <varka.sinitsckaya@yandex.ru>
2022-11-07 13:49:14 +0300

    Change formatting

--- GeneralComponents/GausMethod.cs
+++ GeneralComponents/GausMethod.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 
-namespace GeneralComponents
-{
-    public class GausMethod
-    {
-        public static Matrix2D Solve(Matrix2D coefs, Matrix2D answers)
-        {
+namespace GeneralComponents {
+    public class GausMethod {
+        public static Matrix2D Solve(Matrix2D coefs, Matrix2D answers) {
             if (answers.Columns != 1 || answers.Rows != coefs.Rows)
-                throw new ArgumentException("Can't solve a linear system! Matrix dimensions isn't same!");
+                throw new ArgumentException("Can't solve a linear system! Matrix2D dimensions isn't same!");
 
             if (coefs.Rows != coefs.Columns)
                 throw new ArgumentException("Method solve only a square matrix!");
@@ -20,17 +17,15 @@ namespace GeneralComponents
             regularization = regularization.MakeDiag();
             coefsCopy = coefsCopy + regularization;
 
-            for (int i = 0; i < coefsCopy.Rows; ++i)
-            {
+            for (int i = 0; i < coefsCopy.Rows; ++i) {
                 if (IsFullNulls(coefsCopy, i))
-                    throw new ArgumentException("The system haven't one solution!");
+                    throw new ArgumentException("The system don't have one solution!");
 
                 if (Equals(coefsCopy[i, i], 0))
                     if (!TrySwap(coefsCopy, answersCopy, i, i))
                         continue;
 
-                for (int j = i + 1; j < coefs.Rows; ++j)
-                {
+                for (int j = i + 1; j < coefs.Rows; ++j) {
                     double toMul = coefsCopy[j, i] / coefsCopy[i, i];
                     coefsCopy[j, i] = 0;
                     answersCopy[j] -= answersCopy[i] * toMul;
@@ -41,10 +36,9 @@ namespace GeneralComponents
             }
 
             Matrix2D result = new(coefs.Columns, 1);
-            for (int i = result.Rows - 1; i >= 0; i--)
-            {
+            for (int i = result.Rows - 1; i >= 0; i--) {
                 if (Equals(coefsCopy[i, i], 0))
-                    throw new ArgumentException("The system haven't one solution!");
+                    throw new ArgumentException("The system don't have one solution!");
 
                 double divisible = answersCopy[i];
                 for (int j = i + 1; j < coefs.Columns; ++j)
@@ -56,8 +50,7 @@ namespace GeneralComponents
             return result;
         }
 
-        private static bool IsFullNulls(Matrix2D matrix, int curRow)
-        {
+        private static bool IsFullNulls(Matrix2D matrix, int curRow) {
             for (int i = 0; i < matrix.Columns; ++i)
                 if (matrix[curRow, i] != 0)
                     return false;
@@ -65,10 +58,8 @@ namespace GeneralComponents
             return true;
         }
 
-        private static bool TrySwap(Matrix2D coefs, Matrix2D answers, int startRow, int column)
-        {
-            for (int i = startRow; i < coefs.Rows; ++i)
-            {
+        private static bool TrySwap(Matrix2D coefs, Matrix2D answers, int startRow, int column) {
+            for (int i = startRow; i < coefs.Rows; ++i) {
                 if (Equals(coefs[i, column], 0))
                     continue;
 
