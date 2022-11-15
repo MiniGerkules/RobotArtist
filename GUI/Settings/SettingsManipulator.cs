9fc9eb78ff9fff96b44a1786bdd861680bf11b7d
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-11-15 19:56:35 +0300

    Add a new method that returns path to default config

--- GUI/Settings/SettingsManipulator.cs
+++ GUI/Settings/SettingsManipulator.cs
@@ -3,14 +3,19 @@ using System.Reflection;
 
 namespace GUI.Settings {
     internal class SettingsManipulator {
-        public static string defaultConfDir = "configs";
-        public static string defaultFile = "default.json";
+        private static string defaultConfDir = "configs";
+        private static string defaultFile = "default.json";
 
         public static string GetPathToConfigsDir() {
             string initDir = Assembly.GetExecutingAssembly().Location;
             initDir = initDir.Remove(initDir.LastIndexOf(Path.DirectorySeparatorChar));
 
             return Path.Combine(initDir, defaultConfDir);
+        }
+
+        public static string GetPathToDefaultConf() {
+            var pathToConfDir = GetPathToConfigsDir();
+            return Path.Combine(pathToConfDir, defaultFile);
         }
     }
 }
