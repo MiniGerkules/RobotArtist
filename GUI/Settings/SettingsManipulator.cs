83e816ef4038f21162128f05cea0fd04f9089ba7
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-11-15 18:25:30 +0300

    Add a base class for settings reader and writer

---
+++ GUI/Settings/SettingsManipulator.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace GUI.Settings {
+    internal class SettingsManipulator {
+        public static string defaultConfDir = "configs";
+        public static string defaultFile = "default.json";
+
+        public static string GetPathToConfigsDir() {
+            string initDir = Assembly.GetExecutingAssembly().Location;
+            initDir = initDir.Remove(initDir.LastIndexOf(Path.DirectorySeparatorChar));
+
+            return Path.Combine(initDir, defaultConfDir);
+        }
+    }
+}
