21d491ff8e251ce3d4b7a7f9dbf8381e6ab86c60
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-11-15 13:20:22 +0300

    Remove waste methods and unings

--- GUI/Helpers.cs
+++ GUI/Helpers.cs
@@ -1,37 +1,8 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Collections.Generic;
-
-using GeneralComponents;
 
 namespace GUI {
     internal static class Helpers {
-        public static double GetMaxCoefficient(double maxXPic, double maxYPic, double maxXDisp, double maxYDisp) {
-            double k1 = maxXPic / maxXDisp;
-            double k2 = maxYPic / maxYDisp;
-
-            return Math.Max(k1, k2);
-        }
-
-        public static (double, double) GetCenter(double width, double height) {
-            return (width / 2, height / 2);
-        }
-
-        /// <summary>
-        /// The method finds the canvas center. Takes into account the size of the placed picture
-        /// </summary>
-        /// <param name="canWidth"> canvas width </param>
-        /// <param name="canHeight"> canvas height </param>
-        /// <param name="picWidth"> picture width  </param>
-        /// <param name="picHeight"> picture height </param>
-        /// <returns> Point2D where the center is located </returns>
-        public static Point2D GetCenter(double canWidth, double canHeight, double picWidth, double picHeight) {
-            var (centerX, centerY) = GetCenter(canWidth - picWidth, canHeight - picHeight);
-
-            return new((uint)centerX, (uint)centerY);
-        }
-
         public static string GetFileName(string fullFilePath) {
             string fileName = new(fullFilePath);
 
