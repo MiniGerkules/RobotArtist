2ca11d0029dd9e9fe7d3776eb3f66d2fc09d7512
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-11-15 18:26:37 +0300

    Add the class that writes settings into a file

---
+++ GUI/Settings/SettingsWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Microsoft.Win32;
+
+namespace GUI.Settings {
+    internal class SettingsWriter : SettingsManipulator {
+        public static bool WriteSettings(AlgorithmSettings settings) {
+            SaveFileDialog saveDialog = new() {
+                FileName = $"{DateTime.Now:dd/mm/yyyy hh_mm}",
+                DefaultExt = ".json",
+                Filter = "Algorithm settings|*.json",
+                InitialDirectory = GetPathToConfigsDir(),
+            };
+
+            if (saveDialog.ShowDialog() != true) return false;
+
+            Dictionary<string, double> vals = new();
+            var props = typeof(AlgorithmSettings).GetProperties();
+            foreach (var prop in props)
+                vals.Add(prop.Name, (double)prop.GetValue(settings));
+
+            var options = new JsonSerializerOptions() { WriteIndented = true };
+            var json = JsonSerializer.Serialize(vals, options);
+            File.WriteAllText(saveDialog.FileName, json);
+
+            return true;
+        }
+    }
+}
