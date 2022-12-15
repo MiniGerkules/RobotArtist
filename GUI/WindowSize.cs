7d2f0bb493c1d234f52d06b6699f83a6e07603f2
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-12-15 11:21:36 +0300

    Add a new class that describes sizes of a working window

---
+++ GUI/WindowSize.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GUI {
+    public class WindowSize {
+        private readonly double windowWidth;
+        private readonly double windowHeight;
+
+        public WindowSize(double windowWidth, double windowHeight) {
+            (this.windowWidth, this.windowHeight) = (windowWidth, windowHeight);
+        }
+
+        public double CountScaling(double guiObjWidth, double guiObjHeight) {
+            double widthRatio = windowWidth / guiObjWidth;
+            double heightRatio = windowHeight / guiObjHeight;
+
+            return Math.Min(widthRatio, heightRatio);
+        }
+    }
+}
