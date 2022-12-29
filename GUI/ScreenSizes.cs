4de0a858f9af624860546f888e41c6d1cd7a3514
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-12-29 22:22:46 +0300

    Add class that stores primary screen sizes and counts a scaling factor

---
+++ GUI/ScreenSizes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace GUI {
+    public class ScreenSizes {
+        private readonly double width;
+        private readonly double height;
+
+        public ScreenSizes() {
+            width = SystemParameters.PrimaryScreenWidth;
+            height = SystemParameters.PrimaryScreenHeight;
+        }
+
+        public double CountScaling(double guiObjWidth, double guiObjHeight) {
+            double widthRatio = width / guiObjWidth;
+            double heightRatio = height / guiObjHeight;
+
+            return Math.Min(widthRatio, heightRatio);
+        }
+    }
+}
