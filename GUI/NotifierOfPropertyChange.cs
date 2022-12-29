d49212fba008ac0c62e55603d6f579e6d435e9be
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2022-12-29 22:24:57 +0300

    Add an implementation of INotifyPropertyChanged

---
+++ GUI/NotifierOfPropertyChange.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GUI {
+    public class NotifierOfPropertyChange : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
