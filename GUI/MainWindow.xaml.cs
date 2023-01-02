13cc5cd3e07120be9641fd22d6f8ce1e680f5ce9
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-01-02 20:02:37 +0300

    Remove unnecessary using

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Collections.Generic;
 using System.Windows.Media.Imaging;
