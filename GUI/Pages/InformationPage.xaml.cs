60c9302a78208317231b6621d9cf9062458fbd04
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-01-02 19:29:04 +0300

    Add an InformationPage

---
+++ GUI/Pages/InformationPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+using GUI.PLT;
+using GUI.Settings;
+
+namespace GUI.Pages {
+    public partial class InformationPage : UserControl, IPage {
+        private readonly MenuItem menuItem;
+        private readonly Func<PLTPicture?> getActive;
+
+        MenuItem IPage.LinkedMenuItem => menuItem;
+        Grid IPage.Container => mainGrid;
+
+        public InformationPage(MenuItem menuItem, Func<PLTPicture?> getActive) {
+            InitializeComponent();
+            
+            this.menuItem = menuItem;
+            this.getActive = getActive;
+        }
+
+        public void SetActive() {
+            var active = getActive();
+            if (active == null) return;
+
+            mainGrid.Visibility = Visibility.Visible;
+            menuItem.Background = DefaultGUISettings.activeButton;
+
+            List<string> infoFields = new(active.Settings.numOfSettings) {
+                $"Width (mm) = {active.Width}.", $"Height (mm) = {active.Height}."
+            };
+            foreach (var pair in active.Settings) {
+                string text = AlgorithmSettings.GetPropertyDesc(pair.Item1) + " = " + pair.Item2.ToString();
+                infoFields.Add(text);
+            }
+
+            ShowInfo(infoFields.ToImmutableList());
+        }
+
+        private void ShowInfo(ImmutableList<string> info) {
+            StringBuilder builder = new();
+            foreach (var infoField in info)
+                builder.AppendLine(infoField);
+
+            infoDisplayer.Text = builder.ToString();
+        }
+    }
+}
