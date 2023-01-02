e76d97bd8a1e8c15d2846c534cddb62ca3e8cc53
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-01-02 19:28:05 +0300

    Add a ViewPage

---
+++ GUI/Pages/ViewPage/OpenedFile.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GUI.Pages.ViewPage {
+    public partial class OpenedFile : UserControl {
+        public delegate void ChooseClick(OpenedFile fileToChoose);
+        public delegate void CloseClick(OpenedFile fileToClose);
+
+        private readonly ChooseClick chooseClick;
+        private readonly CloseClick closeClick;
+
+        public string PathToFile { get; }
+
+        public OpenedFile(string pathToFile, ChooseClick chooseClick,
+                          CloseClick closeClick) {
+            InitializeComponent();
+            PathToFile = pathToFile;
+            choose.Content = Path.GetFileName(pathToFile);
+
+            this.chooseClick = chooseClick;
+            this.closeClick = closeClick;
+        }
+
+        private void Choose_Click(object sender, RoutedEventArgs e) {
+            chooseClick(this);
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e) {
+            closeClick(this);
+        }
+    }
+}
