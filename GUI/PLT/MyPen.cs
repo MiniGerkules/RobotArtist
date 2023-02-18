dd5ca2db39e8b75d9d4f65e28351bd4d31819f65
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-02-18 16:44:56 +0300

    Add a new class that helps with updating a pen when color or width is changed [PLT/MyPen.cs]

---
+++ GUI/PLT/MyPen.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Media;
+
+namespace GUI.PLT {
+    internal class MyPen {
+        private Brush brush = Brushes.White;
+        private double thickness = 3;
+
+        public Pen Pen { get; private set; }
+        public Brush Brush {
+            get => brush;
+            set {
+                brush = value;
+                UpdatePen();
+            } 
+        }
+        public double Thickness {
+            get => thickness;
+            set {
+                thickness = value;
+                UpdatePen();
+            }
+        }
+
+        public MyPen() { Pen = new(Brush, Thickness); }
+
+        public MyPen(Brush brush, uint thickness, double scale) {
+            Brush = brush;
+            Thickness = thickness;
+            Pen = new(Brush, Thickness);
+        }
+
+        private void UpdatePen() {
+            Pen = new() {
+                Thickness = Thickness,
+                StartLineCap = PenLineCap.Round,
+                EndLineCap = PenLineCap.Round,
+                Brush = Brush,
+            };
+    }
+    }
+}
