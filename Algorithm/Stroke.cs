3d3dd45d220ea405f332abd52a860dfdc08d03b5
SinBar <varka.sinitsckaya@yandex.ru>
2023-04-01 16:28:36 +0300

    changed to class + modified

--- Algorithm/Stroke.cs
+++ Algorithm/Stroke.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using GeneralComponents;
 
 namespace Algorithm
 {
-    public struct Stroke
+    public class Stroke
     {
         public List<System.Windows.Point> points;
         public double[] color;
-        double[] col8paints;
-        double[] proportions;
-        ColorMixType mixType;
+        public double[] col8paints;
+        public double[] proportions;
+        public ColorMixType mixType;
         public double length = 0;
         public Stroke(System.Windows.Point point, double[] col, double[] col8paints, 
             double[] proportions, ColorMixType mixType, int length = 0)
         {
-            points = new List<System.Windows.Point>();
-            points.Add(point);
+            points = new List<System.Windows.Point>{ point };
             color = col;
             this.col8paints = col8paints;
             this.proportions = proportions;
