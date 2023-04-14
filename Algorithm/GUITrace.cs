d49ec0148d7ab3916c9ac32e80f2a580a3b38ac8
SinBar <varka.sinitsckaya@yandex.ru>
2023-04-14 18:32:06 +0300

    use right constructor

--- Algorithm/GUITrace.cs
+++ Algorithm/GUITrace.cs
@@ -5,35 +5,16 @@ namespace Algorithm
 {
     public struct GUITrace // GUI_Trace
     {
-        // this constructor sholdn't be used, use constructor with parameters
-        public GUITrace() // just some parameters to avoid fatal errors // DELETE THIS AFTER ALL IS DONE
-        {
-            this.colorsAmount = 255;
-            this.brushWidthMM = 2;
-            this.canvasWidthMM = 10; //390; 
-            this.canvasHeightMM = 10; //290;
-        }
         public double brushWidthMM { get; private set; } // #brushWidth_mm
         public double canvasWidthMM { get; private set; } // #canvasW_mm
         public double canvasHeightMM { get; private set; } // #canvasH_mm
-        //public uint usespp { get; private set; } // всегда PP, не PC - кодирование в пропорциях красок
         public uint colorsAmount { get; private set; } // amount of colors in the final picture // nColors
-        public GUITrace(uint colorsAmount = 256, params double[] sizesListMM) // is that ok to set colorsAmount to 256?
+        public GUITrace(uint colorsAmount = 255, double brushWidthMM = 2, double canvasWidthMM = 10, double canvasHeightMM = 10)
         {
             this.colorsAmount = colorsAmount;
-            this.brushWidthMM = 0;
-            this.canvasWidthMM = 0;
-            this.canvasHeightMM = 0;
-            if (sizesListMM != null)
-            {
-                if (sizesListMM.Length >= 3)
-                {
-                    this.brushWidthMM = sizesListMM[0];
-                    this.canvasWidthMM = sizesListMM[1];
-                    this.canvasHeightMM = sizesListMM[2];
-                }
-                else throw new ArgumentException("sizes for brush width and canvas width and height should be done!");
-            }
+            this.brushWidthMM = brushWidthMM;
+            this.canvasWidthMM = canvasWidthMM;
+            this.canvasHeightMM = canvasHeightMM;
         }
     }
 }
