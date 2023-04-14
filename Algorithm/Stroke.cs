d4d10188453da9f183d6c0eb7169e1e8b09ea0bd
SinBar <varka.sinitsckaya@yandex.ru>
2023-04-14 18:34:00 +0300

    added function to convert algorithm strokes to plt strokes

--- Algorithm/Stroke.cs
+++ Algorithm/Stroke.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using GeneralComponents;
+using GeneralComponents.Colors;
 
 namespace Algorithm
 {
@@ -20,6 +22,50 @@ namespace Algorithm
             this.proportions = proportions;
             this.mixType = mixType;
             this.length = length;
+        }
+
+        public List<GeneralComponents.PLT.Stroke> ConvertToPLTStroke(IColor icolor, double brushWidth)
+        {
+            if (icolor is RGBColor)
+            {
+                if (points.Count == 1)
+                    return new List<GeneralComponents.PLT.Stroke>()
+                { new GeneralComponents.PLT.Stroke(
+                    new Point2D(
+                        (uint)points[0].X,
+                        (uint)points[0].Y),
+                    new Point2D(
+                        (uint)points[0].X,
+                        (uint)points[0].Y),
+                    new RGBColor((byte)color[0], (byte)color[1], (byte)color[2]),
+                    brushWidth) };
+                else
+                {
+                    var answer = new List<GeneralComponents.PLT.Stroke>();
+                    for (int i = 1; i < points.Count; i++)
+                    {
+                        answer.Add(
+                            new GeneralComponents.PLT.Stroke(
+                            new Point2D(
+                                (uint)points[i - 1].X,
+                                (uint)points[i - 1].Y),
+                            new Point2D(
+                                (uint)points[i].X,
+                                (uint)points[i].Y),
+                        new RGBColor((byte)color[0], (byte)color[1], (byte)color[2]),
+                        brushWidth));
+                    }
+                    return answer;
+                }
+            }
+            else if (icolor is CMYBWColor)
+            {
+                // how to create this??? 
+                // 1 color = 8 numbers where every number is a proportion of color
+                throw new NotImplementedException();
+            }
+            else
+                throw new ArgumentException();
         }
     }
 
