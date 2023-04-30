fac46aead877e015e3d26a229d3fd41e3f6796b0
SinBar <varka.sinitsckaya@yandex.ru>
2023-05-01 00:48:04 +0300

    little fixes

--- Algorithm/StrokeBrush.cs
+++ Algorithm/StrokeBrush.cs
@@ -9,13 +9,13 @@ namespace Algorithm
         public int smallThickness { get; private set; } // #bs2
         public int maxStrokeLength { get; private set; } // #maxLen
         public double bsQuad { get; private set; } // #bsQuad
-        public StrokeBrush(double brushWidthMM, double sfX) // sfX is a coefficient <= 1
+        public StrokeBrush(double brushWidthMM, double sfX, int maxLenFactor) // sfX is a coefficient <= 1
         {
             //try
             //{
                 this.thickness = Math.Max(2, (int)Math.Round(brushWidthMM / sfX));
                 this.smallThickness = Math.Max(1, (int)Math.Ceiling((double)thickness / 2));
-                this.maxStrokeLength = thickness * 10;
+                this.maxStrokeLength = thickness * maxLenFactor;
                 this.bsQuad = ((double)thickness / 2d) * ((double)thickness / 2d); // is that ok?
             //}
             //catch (ArgumentException e)
